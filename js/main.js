e9df70227ed0a670c375bb30e73b6ddb775514e2
cigaretteprose <erslamii@gmail.com>
2026-05-08 23:23:53 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('For the one reading this, I hope life has been kind to you lately.').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
