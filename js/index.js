e34db00d290120f90eb1b0c2c7663ec8f309e272
cigaretteprose <erslamii@gmail.com>
2026-05-08 23:20:57 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `HERES LITTLE GIFT FROM PRADYA`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
